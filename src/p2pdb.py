36a0a320568e6feee5b5cf10f766e96a1c270472
dpaganel <danpags15@gmail.com>
2023-03-28 10:21:17 -0400

    Function Stumps Implemented
    Added the basic function stumps that will be implemented for the
    database. These will allow for the saving of messages and the rereading
    of a conversation.

---
+++ src/p2pdb.py
@@ -0,0 +1,30 @@
+# Database module utilizing sqlite3 for the peer to peer client
+# Project
+import sqlite3
+
+
+# Initialize the database, if it hasn't been created already
+def createDB():
+
+    return 0
+
+# Save a recieved message along with a timestamp, the user who made it,
+# and a session id (?)
+def saveRcvMsg():
+
+    return 0
+
+# Save a message we made to the database
+def saveSentMsg():
+    
+    return 0
+
+# Print out all of the messages in a session
+def printSession():
+
+    return 0
+
+# Print out all of the messages to and from a user
+def printUserMsgs():
+
+    return 0
