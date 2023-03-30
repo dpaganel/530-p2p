2cca503f7b7ab51709a0cb7a76f7f5fe8ba3db87
kmilgrim <kmilgrim@bu.edu>
2023-03-30 10:06:59 -0400

    this code works over hotspot :)

--- p2p-chat-code.py
+++ p2p-chat-code.py
@@ -8,20 +8,20 @@ import threading
 # recv_port = int(input("recv port: "))
 
 # hard coded right now, could get input from user or system though! ^^
-send_ip = '192.168.56.1'
-send_port = 4444
-recv_ip = '10.0.2.15'
-recv_port = 2222
+my_ip = '172.20.10.2'
+my_port = 4444
+receiver_ip = '172.20.10.10'
+receiver_port = 4444
 
 # bind port to socket on current platform
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((send_ip, send_port))
+s.bind((my_ip, my_port))
 
 # define send functionality, encode message and send to receiving ip/port
 def send():
     while True:
         message = input("")
-        s.sendto(message.encode(), (recv_ip, recv_port))
+        s.sendto(message.encode(), (receiver_ip, receiver_port))
         print(s)
 
 # define receiving functionality, decode message and print to screen
