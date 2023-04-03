f90e88cd005f908cf8ecfd4bbfefbe97c58eaa70
kmilgrim <kmilgrim@bu.edu>
2023-04-03 16:33:19 -0400

    adding acks, currently an infinite loop, not sure how to prevent that yet

--- src/p2p-chat-code.py
+++ src/p2p-chat-code.py
@@ -8,10 +8,12 @@ import threading
 # recv_port = int(input("recv port: "))
 
 # hard coded right now, could get input from user or system though! ^^
-my_ip = '172.20.10.2'
+my_ip = '192.168.56.1'
 my_port = 4444
-receiver_ip = '172.20.10.10'
-receiver_port = 4444
+receiver_ip = '192.168.56.1'
+receiver_port = 2345
+
+ack = "code 1: message was sent successfully"
 
 # bind port to socket on current platform
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,7 +30,13 @@ def send():
 def recv():
     while True:
         message_recv = s.recvfrom(1024)
+        message_ack = 1
         print("message received: " + message_recv[0].decode())
+        if (message_recv != "message received: code 1: message was sent successfully"):
+            send_ack()
+
+def send_ack():
+    s.sendto(ack.encode(), (receiver_ip, receiver_port))
 
 # utilize threads to enable sending and receiving concurrently.
 t1 = threading.Thread(target=send)
